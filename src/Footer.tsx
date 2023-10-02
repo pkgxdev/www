c786cc28679faa75569efd145624b4b365ce2fd4
Max Howell <mxcl@me.com>
2023-10-02 10:25:31 -0400

    tweaks

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,8 @@
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import { Link, LinkProps, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { Link, LinkProps, Typography, useTheme, useMediaQuery, Box, Button, Stack } from "@mui/material";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowOutwardIcon from '@mui/icons-material/CallMade';
+import tea from "./assets/wordmarks/tea.svg";
 import logo from "./assets/pkgx.svg";
 
 
@@ -26,48 +28,60 @@ export default function Footer() {
 
   const icon = <ArrowOutwardIcon fontSize="inherit" style={{transform: 'translateY(3px)'}} />
 
-  return <Grid container spacing={2} columns={isxs ? 12 : 11} width='100%'>
-           <Grid xs={12} md={5}>
-             <img src={logo} height={18} />
-             {!isxs && c}
-           </Grid>
-           <Grid xs={4} md={2}>
-             <Typography variant="h5" fontWeight='bold' fontSize={14}>
-               Product
-             </Typography>
-             <ul style={ul_style}>
-               <Li><Link href='https://pkgx.sh' {...link_props}>pkgx</Link></Li>
-               <Li><Link href='https://pkgx.app' {...link_props}>oss.app</Link></Li>
-               <Li><Link href='https://docs.pkgx.sh' {...link_props}>docs</Link></Li>
-               <Li><Link href='https://dist.pkgx.dev' {...link_props}>pkgs</Link></Li>
-             </ul>
-           </Grid>
-           <Grid xs={4} md={2}>
-             <Typography variant="h5" fontWeight='bold' fontSize={14}>
-               Company
-             </Typography>
-             <ul style={ul_style}>
-               <Li><Link href='https://pkgx.dev' {...link_props}>Home</Link></Li>
-               <Li><Link href='https://pkgx.dev/privacy-policy' {...link_props}>Privacy Policy</Link></Li>
-               <Li><Link href='https://pkgx.dev/terms-of-use' {...link_props}>Terms of Use</Link></Li>
-               <Li><Link href='https://blog.pkgx.dev' {...link_props}>Blog</Link></Li>
-               <Li><Link href='mailto:hi@pkgx.dev' {...link_props}>Contact{icon}</Link></Li>
-             </ul>
-           </Grid>
-           <Grid xs={4} md={2}>
-             <Typography variant="h5" fontWeight='bold' fontSize={14}>
-               Community
-             </Typography>
-             <ul style={ul_style}>
-               <Li><Link href='https://github.com/pkgxdev' {...link_props}>GitHub{icon}</Link></Li>
-               <Li><Link href='https://x.com/pkgxdev' {...link_props}>𝕏{icon}</Link></Li>
-               <Li><Link href='https://www.irccloud.com/irc/libera.chat/channel/pkgx' {...link_props}>irc:#pkgx{icon}</Link></Li>
-             </ul>
-           </Grid>
-           {isxs && <Grid xs={12}>
-             {c}
-           </Grid>}
-         </Grid>
+  return <Stack spacing={8}>
+    <Stack direction={isxs ? 'column' : 'row'} spacing={isxs ? 2: 3} alignItems='center' sx={{"&&": {mt: 16}}}>
+      <Box component='img' src={tea} height='20px' />
+      <Typography>
+        pkgx is a core contributor to the tea protocol
+      </Typography>
+      <Button variant='outlined' color='inherit' href='https://tea.xyz' size='small' endIcon={<ArrowForwardIcon />}>
+        Learn More
+      </Button>
+    </Stack>
+
+    <Grid container spacing={2} columns={isxs ? 12 : 11} width='100%'>
+      <Grid xs={12} md={5}>
+        <img src={logo} height={18} />
+        {!isxs && c}
+      </Grid>
+      <Grid xs={4} md={2}>
+        <Typography variant="h5" fontWeight='bold' fontSize={14}>
+          Product
+        </Typography>
+        <ul style={ul_style}>
+          <Li><Link href='https://pkgx.sh' {...link_props}>pkgx</Link></Li>
+          <Li><Link href='https://pkgx.app' {...link_props}>oss.app</Link></Li>
+          <Li><Link href='https://docs.pkgx.sh' {...link_props}>docs</Link></Li>
+          <Li><Link href='https://dist.pkgx.dev' {...link_props}>pkgs</Link></Li>
+        </ul>
+      </Grid>
+      <Grid xs={4} md={2}>
+        <Typography variant="h5" fontWeight='bold' fontSize={14}>
+          Company
+        </Typography>
+        <ul style={ul_style}>
+          <Li><Link href='https://pkgx.dev' {...link_props}>Home</Link></Li>
+          <Li><Link href='https://pkgx.dev/privacy-policy' {...link_props}>Privacy Policy</Link></Li>
+          <Li><Link href='https://pkgx.dev/terms-of-use' {...link_props}>Terms of Use</Link></Li>
+          <Li><Link href='https://blog.pkgx.dev' {...link_props}>Blog</Link></Li>
+          <Li><Link href='mailto:hi@pkgx.dev' {...link_props}>Contact{icon}</Link></Li>
+        </ul>
+      </Grid>
+      <Grid xs={4} md={2}>
+        <Typography variant="h5" fontWeight='bold' fontSize={14}>
+          Community
+        </Typography>
+        <ul style={ul_style}>
+          <Li><Link href='https://github.com/pkgxdev' {...link_props}>GitHub{icon}</Link></Li>
+          <Li><Link href='https://x.com/pkgxdev' {...link_props}>𝕏{icon}</Link></Li>
+          <Li><Link href='https://www.irccloud.com/irc/libera.chat/channel/pkgx' {...link_props}>irc:#pkgx{icon}</Link></Li>
+        </ul>
+      </Grid>
+      {isxs && <Grid xs={12}>
+        {c}
+      </Grid>}
+    </Grid>
+  </Stack>
 }
 
 function Li({ children }: { children: React.ReactNode }) {
