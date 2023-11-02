66e48d1b48fe4a724c097319ede77b6fbe54d08d
Max Howell <mxcl@me.com>
2023-11-02 12:24:46 -0400

    wip

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,8 +1,12 @@
 import { Stack, IconButton, Box, Tooltip, Typography } from "@mui/material";
-import { useAsync } from "react-use";
 import github from "../assets/wordmarks/github.svg";
+import {useLocation } from "react-router-dom";
+import { useAsync } from "react-use";
 
 export default function Stars() {
+  const path = useLocation();
+  const dst = path.pathname.split('/')[0] == 'pkgs' ? 'pantry' : 'pkgx';
+
   const {value: stars} = useAsync(async () => {
     const response = await fetch('https://pkgx.sh/stars.json');
     const data = await response.json();
@@ -10,7 +14,7 @@ export default function Stars() {
   }, [])
 
   return <Stack spacing={0} direction='row' alignItems='center'>
-    <IconButton href='https://github.com/pkgxdev/pkgx'>
+    <IconButton href={`https://github.com/pkgxdev/${dst}`}>
       <Box component='img' src={github}/>
     </IconButton>
     <Tooltip title='Total Org. Stars' arrow placement='right' enterTouchDelay={0}>
