e5f76162d588c64d54108d3dd22243a5f8b8ac35
michaelessiet <emsaa2002@gmail.com>
2023-11-27 22:05:13 +0100

    ...

--- src/pkgx.sh/Hero.tsx
+++ src/pkgx.sh/Hero.tsx
@@ -27,36 +27,29 @@ export default function Hero() {
     ? { minWidth: 'fit-content', height: 'fit-content', padding: 12 }
     : undefined
 
-  return (
-    <Stack
-      spacing={6}
-      textAlign='center'
-      mx='auto'
-      alignItems='center'
-      sx={isxs ? undefined : { '&&': { mt: 22 } }}
-    >
-      <HeroTypography>
-        Run Anything
-      </HeroTypography>
+  return <Stack spacing={6} textAlign='center' mx='auto' alignItems='center' sx={isxs ? undefined : {"&&": {mt: 22}}}>
+    <HeroTypography>
+      Run Anything
+    </HeroTypography>
 
-      <Typography variant="h5" px={1} sx={{"&&": {mt: 6.5}}}  maxWidth={isxs ? undefined : 570}>
-        <code>pkgx</code> is a blazingly fast, standalone, cross‐platform binary that <i>runs anything</i>
-      </Typography>
+    <Typography variant="h5" px={1} sx={{"&&": {mt: 6.5}}}  maxWidth={isxs ? undefined : 570}>
+      <code>pkgx</code> is a blazingly fast, standalone, cross‐platform binary that <i>runs anything</i>
+    </Typography>
 
-      <Box px={isxs ? undefined : 10} width={isxs ? '90vw' : 570}>
-        <Tooltip title="Click to Copy" placement='right' arrow>
-          <TextField
-            onClick={click}
-            className="halo"
-            value={text()}
-            fullWidth={true}
-            InputProps={{
-              endAdornment: <InputAdornment position="end"><ContentCopyIcon /></InputAdornment>,
-              readOnly: true,
-              style: {cursor: 'default', fontFamily: 'monospace', fontSize: isxs ? 14 : undefined},
-            }}
-          />
-        </Tooltip>
+    <Box px={isxs ? undefined : 10} width={isxs ? '90vw' : 570}>
+      <Tooltip title="Click to Copy" placement='right' arrow>
+        <TextField
+          onClick={click}
+          className="halo"
+          value={text()}
+          fullWidth={true}
+          InputProps={{
+            endAdornment: <InputAdornment position="end"><ContentCopyIcon /></InputAdornment>,
+            readOnly: true,
+            style: {cursor: 'default', fontFamily: 'monospace', fontSize: isxs ? 14 : undefined},
+          }}
+        />
+      </Tooltip>
 
       <Snackbar open={open} autoHideDuration={1500} onClose={close} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
         <Alert onClose={close} severity="success" variant='filled' color={'primary' as any}>
@@ -64,28 +57,27 @@ export default function Hero() {
         </Alert>
       </Snackbar>
 
-        <Tabs
-          value={searchParams.get('via')?.toLowerCase()}
-          onChange={(_, val) => setSearchParams({ via: val })}
-          style={{ paddingInline: '0.5em' }}
-        >
-          <Tab label='brew' value='brew' />
-          <Tab label='cURL' value='curl' />
-          <Box width='100%'>
-            <Stack direction='row' justifyContent='end' mt={1}>
-              <Button
-                href='https://docs.pkgx.sh/installing-w/out-brew'
-                sx={{ mt: 0.5 }}
-                size='small'
-                color='inherit'
-                endIcon={<ArrowForwardIcon />}
-              >
-                other ways {isxs || 'to install'}
-              </Button>
-            </Stack>
-          </Box>
-        </Tabs>
-      </Box>
-    </Stack>
-  )
+      <Tabs
+        value={searchParams.get('via')?.toLowerCase()}
+        onChange={(_, val) => setSearchParams({ via: val })}
+        style={{ paddingInline: '0.5em' }}
+      >
+        <Tab label='brew' value='brew' />
+        <Tab label='cURL' value='curl' />
+        <Box width='100%'>
+          <Stack direction='row' justifyContent='end' mt={1}>
+            <Button
+              href='https://docs.pkgx.sh/installing-w/out-brew'
+              sx={{ mt: 0.5 }}
+              size='small'
+              color='inherit'
+              endIcon={<ArrowForwardIcon />}
+            >
+              other ways {isxs || 'to install'}
+            </Button>
+          </Stack>
+        </Box>
+      </Tabs>
+    </Box>
+  </Stack>
 }
