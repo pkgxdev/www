a0e0699f68ce0f796b66f39f6f5c7264f47a98a4
Max Howell <mxcl@me.com>
2025-02-03 11:38:33 -0500

    Attempt to use DeepSeek to craft the img features

--- .github/scripts/gen-imgs.py
+++ .github/scripts/gen-imgs.py
@@ -10,9 +10,11 @@
 # modal https://civitai.com/models/293331/wildcardx-xl-turbo?modelVersionId=329685&dialog=resourceReview&reviewId=26068625
 # lora  https://civitai.com/models/118418/negativexl
 
+import sys
+import unicodedata
 from rich.console import Console
+import subprocess
 import requests
-import textwrap
 import base64
 import random
 import json
@@ -31,53 +33,24 @@ def gen_img(root, json_file):
     with open(os.path.join(root, json_file), 'r') as f:
         metadata = json.load(f)
 
-        description = metadata.get('description') or metadata.get('homepage') or metadata.get('project')
+        output = get_painting_feature(metadata.get('displayName'), metadata.get('description'))
+        feature = output.get('theme')
+        imagery = output.get('imagery')
+        colors = output.get('colors')
 
-        if metadata.get('displayName'):
-            description = f"{metadata['displayName']}: {description}"
-
-        color_suggestions = [
-            "black, white, orange, beige, and pink",
-            "Charcoal Gray, Off-White, Olive Green, Burnt Sienna, and Blush Pink",
-            "Pearl White, Crimson Red, Champagne, and Pale Peach",
-            "Forest Green, Ivory, Coral Orange, Sand Beige, and Dusty Rose",
-            "Espresso Brown, Creamy White, Golden Yellow, Taupe, and Mauve",
-            "Jet Black, Soft Gray, Teal, Warm Amber, and Powder Pink",
-        ]
-
-        # pick random entry from colors
-        colors = random.choice(color_suggestions)
-
-        imagery_suggestions = [
-            ["sky", "flowers", "water", "fire"],
-            ["mountains", "valleys", "rivers", "meadows"],
-            ["forests", "lakes", "wildlife", "sunlight"],
-            ["clouds", "storms", "rainbows", "snow"],
-            ["deserts", "cacti", "sand dunes", "sunsets"],
-            ["stars", "moons", "galaxies", "nebulae"],
-            ["oceans", "waves", "tides", "coral reefs"],
-            ["volcanoes", "lava", "ash", "craters"],
-            ["glaciers", "icebergs", "frost", "polar lights"],
-            ["caves", "stalactites", "stalagmites", "echoes"],
-            ["prairies", "grasslands", "buffalo", "sunrises"],
-            ["jungles", "vines", "canopies", "tropical birds"],
-            ["cliffs", "canyons", "gorges", "waterfalls"],
-            ["auroras", "meteors", "comets", "cosmic dust"],
-            ["reefs", "atolls", "tropica" "fish", "sea turtles"],
-            ["twilight", "dawn", "dusk", "moonlight"],
-        ]
-
-        imagery = ", ".join(random.choice(imagery_suggestions))
+        if isinstance(imagery, list):
+            imagery = ", ".join(imagery)
+        if isinstance(colors, list):
+            colors = ", ".join(colors)
 
         seed = random.randint(0, 2**32 - 1)
 
         prompt = f"""
-        Create an abstract oil painting that features or represents:
-        {description.strip()}.
-        Use an expressive explosion of {colors}—with and hyper-detailed textures.
+        Create an abstract oil painting that features or represents: {feature}.
+        Use an expressive explosion of {colors} and hyper-detailed textures.
         Highlight the artwork with gleaming golden accents that radiate light amidst a brilliance of harmony.
         Incorporate ethereal elements like {imagery} to symbolize the peaceful blending of these forces.
-        Ensure a perfect composition with intricate pearl filigree, capturing a serene and radiant ambiance
+        Ensure a perfect composition with intricate pearl filigree, capturing a serene and radiant ambiance.
         """
 
         prompt = re.sub(r'\s+', ' ', prompt).strip()
@@ -115,6 +88,128 @@ def gen_img(root, json_file):
 
         console.print(f"Done {out_file}", style="bold green")
 
+def get_painting_feature(name, description):
+    prompt = "I am making a painting about an open source package"
+    if name:
+        prompt += f" called “{name}”"
+    if description:
+        prompt += f" which is described as “{description}”"
+
+    prompt += f"""
+I am generating images to represent different open source packages.
+I need a theme. It should at most be two or three words.
+This is an image being generated as part of over 10 million other packages so the theme should try to be unique and clearly linked to the package in question.
+If the package name contains a suitable word use it, eg. if the package is called foocat, then say cat.
+If the package name could be interpreted for a painting use it, eg. if the package is called supercat, then say SUPER CAT, eg. if the package is called trimonkey say THREE MONKEYS.
+If the package name is a play on words extract the meaning, eg. if the package is called wompbat because it is a library for womp, return WOMBAT.
+Do not go conceptual with eg. code or syntax since there are 10 million images and most of them are related to code, syntax and other technological themes.
+If the package description contains a word that evokes something, then use it, eg “foo is bridge to bar” then say BRIDGE.
+Be metaphorical, eg. if the package is a database and no other theme comes to mind, suggest shelves or boxes.
+I trust you. Use your judgement. You’re very smart and capable.
+The painting either features the theme or conceptualizes it. In both cases it must be something that can be painted.
+I need the output as JSON "{{theme: ""}}.
+I am feeding the output to a script, so if you don’t output json in the above form my script will break.
+Thank you sir.
+"""
+    prompt = re.sub(r'\s+', ' ', prompt).strip()
+
+    print(prompt)
+
+    proc = subprocess.run(['pkgx', 'ollama', 'run', 'deepseek-r1', f"Instruct: {prompt}\nOutput: "], capture_output=True, text=True)
+    result = proc.stdout
+
+    console.print(result, style="yellow")
+
+    result = result[result.index("</think>") + len("</think>"):].strip()
+
+    output = {}
+
+    match = re.search(r'{\s*"?theme"?:(.*?)}', result, re.DOTALL)
+    if match:
+        output['theme'] = match.group(1)
+    else:
+        # sometimes it just outputs the theme. Often with surrounding description, but whatever
+        output['theme'] = re.sub(r'\s+', ' ', result).strip()
+
+    prompt = f"""
+I am making an abstract landscape oil painting with the theme "{output['theme']}".
+I need an appropriate color palette. Pick one of these:
+
+```markdown
+- black, white, orange, beige, and pink
+- Charcoal Gray, Off-White, Olive Green, Burnt Sienna, and Blush Pink
+- Pearl White, Crimson Red, Champagne, and Pale Peach
+- Forest Green, Ivory, Coral Orange, Sand Beige, and Dusty Rose
+- Espresso Brown, Creamy White, Golden Yellow, Taupe, and Mauve
+- Jet Black, Soft Gray, Teal, Warm Amber, and Powder Pink
+```
+
+If a theme requires it, adapt the color palette (ideally minimally), eg. if the theme is “Ruby” then add the color ruby!
+I need the output as JSON "{{colors: ""}}.
+Output the complete list item of colors, not just parts.
+I am feeding the output to a script, so if you don’t output json in the above form my script will break.
+Thank you sir.
+"""
+    print(prompt)
+
+    prompt = f"Instruct: {prompt}\nOutput: "
+
+    proc = subprocess.run(['pkgx', 'ollama', 'run', 'deepseek-r1', prompt], capture_output=True, text=True)
+    result = proc.stdout
+
+    console.print(result, style="blue")
+
+    result = result[result.index("</think>") + len("</think>"):].strip()
+
+    match = re.search(r'{\s*"?colors"?:(.*?)}', result, re.DOTALL)
+    if match:
+        output['colors'] = match.group(1)
+    else:
+        # sometimes it just outputs the theme. Often with surrounding description, but whatever
+        output['colors'] = re.sub(r'\s+', ' ', result).strip()
+
+    prompt = f"""
+I have a theme: {output['theme']} and colors: {output['colors']}.
+Now pick from following list of imagery options to best fit that theme and color selection:
+
+```markdown
+- oceans, waves, tides, coral reefs
+- volcanoes, lava, ash, craters
+- glaciers, icebergs, frost, polar lights
+- caves, stalactites, stalagmites, echoes
+- prairies, grasslands, sunrises
+- jungles, vines, canopies
+- cliffs, canyons, gorges, waterfalls
+- auroras, meteors, comets, cosmic dust, moons, planetscape
+- reefs, atolls, tropica
+- twilight, dawn, dusk, moonlight
+```
+
+If none fit and you are inspired please provide your own imagery or adapt the options.
+I need the output as JSON "{{imagery: ""}}.
+Output the complete list item of imagery items, not just parts.
+I am feeding the output to a script, so if you don’t output json in the above form my script will break.
+Thank you sir.
+"""
+    print(prompt)
+
+    prompt = f"Instruct: {prompt}\nOutput: "
+
+    proc = subprocess.run(['pkgx', 'ollama', 'run', 'deepseek-r1', prompt], capture_output=True, text=True)
+    result = proc.stdout
+
+    console.print(result, style="green")
+
+    result = result[result.index("</think>") + len("</think>"):].strip()
+
+    match = re.search(r'{\s*"?imagery"?:(.*?)}', result, re.DOTALL)
+    if match:
+        output['imagery'] = match.group(1)
+    else:
+        # sometimes it just outputs the theme. Often with surrounding description, but whatever
+        output['imagery'] = re.sub(r'\s+', ' ', result).strip()
+
+    return output
 
 url = "http://127.0.0.1:7860/sdapi/v1/txt2img"
 console = Console()
